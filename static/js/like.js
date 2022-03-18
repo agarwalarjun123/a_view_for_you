8b8a173240901a55b9ab782d86fe5135d02e9e65
Luoxuan Peng <2668528p@student.gla.ac.uk>
2022-03-18 19:40:19 +0000

    modify detail style

---
+++ static/js/like.js
@@ -0,0 +1,8 @@
+ $(document).ready(function () {
+            $('.content').click(function () {
+                $('.content').toggleClass("heart-active")
+                $('.text').toggleClass("heart-active")
+                $('.numb').toggleClass("heart-active")
+                $('.heart').toggleClass("heart-active")
+            });
+        });
