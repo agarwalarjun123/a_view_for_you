3a79fe2f349005dc41cb75d5576b0a3b1914bc48
arjun Agarwal <arjunagarwal1998@gmail.com>
2022-03-19 23:55:57 +0530

    add like

--- static/js/like.js
+++ static/js/like.js
@@ -1,8 +1,26 @@
  $(document).ready(function () {
+            
             $('.content').click(function () {
-                $('.content').toggleClass("heart-active")
-                $('.text').toggleClass("heart-active")
-                $('.numb').toggleClass("heart-active")
-                $('.heart').toggleClass("heart-active")
+
+                const url = new URL(window.location.href)
+                const slug = _.last(url.pathname.split('/').filter((path) =>!_.isEmpty(path)))
+                const liked = $(".content").hasClass('heart-active') ? false: true;
+                $.ajax({
+                    url: `/landscape/${slug}/like`,
+                    method: "PUT",
+                    dataType: "json",
+                    contentType : 'application/json',
+                    data : JSON.stringify({
+                        liked,
+                    }),
+                    error: (xhr, status, error) => {},
+                    success: (data) => {
+                        $('.content').toggleClass("heart-active")
+                        $('.text').toggleClass("heart-active")
+                        $('.numb').toggleClass("heart-active")
+                        $('.heart').toggleClass("heart-active")
+                      }
+                  });
+               
             });
         });
