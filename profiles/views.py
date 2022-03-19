2b2d178ce2afd483fbfb05571a72d7e71ec0a778
Luoxuan Peng <2668528p@student.gla.ac.uk>
2022-03-19 23:14:51 +0000

    fix like function error

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
-from landscape.models import Like, Review
+from landscape.models import saved_landscapes, Review
 from landscape.views import roundRating
 from django.contrib.auth.decorators import login_required
 
 @login_required()
 def show_profile(request):
+    context_dict = {}
     if request.method == 'GET':
-        return render(request, 'profile/profile.html')
+        try:
+            profile_likes = saved_landscapes.objects.filter(user_id_id=request.user.id)
+            context_dict['profile_likes'] = profile_likes
+        except saved_landscapes.DoesNotExist:
+            # do nothing
+            context_dict['profile_likes'] = None
 
+        try:
+            profile_reviews = Review.objects.filter(
+                user_id=request.user.id).order_by('-visit_date')
+            for r in profile_reviews:
+                r.rating = roundRating(r.rating)
+            context_dict['profile_reviews'] = profile_reviews
+        except Review.DoesNotExist:
+            # do nothing
+            context_dict['profile_reviews'] = None
+    return render(request, 'profile/profile.html', context=context_dict)
+    #return render(request, 'profile/profile.html')
 
 # try to filter reviews made by user
 @login_required()
