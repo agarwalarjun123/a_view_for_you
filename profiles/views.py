6f7a8cc89a02fe6f960b48c5d90c05bc140b9359
arjun Agarwal <arjunagarwal1998@gmail.com>
2022-03-20 09:44:21 +0530

    merge fix

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from landscape.models import saved_landscapes, Review
-from landscape.views import roundRating
+from utils.utils import roundRating
 from django.contrib.auth.decorators import login_required
 
 @login_required()
