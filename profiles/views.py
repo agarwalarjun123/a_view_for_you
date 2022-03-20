fc8419447cbd19b439cbd5a0d1e7b72cf39212fa
Zhehan Hu <2357914H@student.gla.ac.uk>
2022-03-20 03:57:36 +0000

    fix profile likes, link back to landscape

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
+import this
 from django.shortcuts import render
-from landscape.models import Like, Review
+from landscape.models import Landscape, saved_landscapes, Review, Like
 from landscape.views import roundRating
 from django.contrib.auth.decorators import login_required
 
@@ -7,24 +8,26 @@ from django.contrib.auth.decorators import login_required
 def show_profile(request):
     if request.method == 'GET':
         context_dict = {}
+        profile_likes = set()
         try:
-            profile_likes = Like.objects.filter(
+            profile_saved_landscapes = saved_landscapes.objects.filter(
                 user_id=request.user.id)
+            for like in profile_saved_landscapes:
+                #like.landscape_id is not a integer but a Landscape instance!
+                profile_likes.add(like.landscape_id)
             context_dict['profile_likes'] = profile_likes
-        except Review.DoesNotExist:
+        except saved_landscapes.DoesNotExist:
             # do nothing
             context_dict['profile_likes'] = None
 
-        context_dict = {}
         try:
             profile_reviews = Review.objects.filter(
                 user_id=request.user.id).order_by('-visit_date')
-            for r in profile_reviews:
-                r.rating = roundRating(r.rating)
             context_dict['profile_reviews'] = profile_reviews
         except Review.DoesNotExist:
             # do nothing
             context_dict['profile_reviews'] = None
+
     return render(request, 'profile/profile.html', context=context_dict)
         #return render(request, 'profile/profile.html')
 
