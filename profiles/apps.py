cebe2478dd7a9f4b14ed5d9672a12f07ac9de2fa
Zhehan Hu <2357914H@student.gla.ac.uk>
2022-03-20 10:40:20 +0000

    remove redundant

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class ProfileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profile'
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,5 @@
-import this
 from django.shortcuts import render
-from landscape.models import Landscape, saved_landscapes, Review, Like
-from landscape.views import roundRating
+from landscape.models import saved_landscapes, Review
 from django.contrib.auth.decorators import login_required
 
 @login_required()
@@ -15,6 +13,7 @@ def show_profile(request):
             for like in profile_saved_landscapes:
                 #like.landscape_id is not a integer but a Landscape instance!
                 profile_likes.add(like.landscape_id)
+            #profile_likes is a set of landscapes
             context_dict['profile_likes'] = profile_likes
         except saved_landscapes.DoesNotExist:
             # do nothing
@@ -29,36 +28,42 @@ def show_profile(request):
             context_dict['profile_reviews'] = None
 
     return render(request, 'profile/profile.html', context=context_dict)
-        #return render(request, 'profile/profile.html')
+    #return render(request, 'profile/profile.html')
 
-# try to filter landscapes liked by user
-@login_required()
-def show_profile_likes(request):
+# try to filter users who likes this landscape
+def show_landscape_likes(request):
     # Create a context dictionary which we can pass
     # to the template rendering engine.
     context_dict = {}
+    landscape_likes = set()
     try:
-        profile_likes = Like.objects.filter(
-            user_id=request.user.id).order_by('-visit_date')
-        context_dict['profile_likes'] = profile_likes
-    except Review.DoesNotExist:
+        saved_landscape_likes = saved_landscapes.objects.filter(
+            landscpae_id=request.landscape.id)
+        for like in saved_landscape_likes:
+            #like.user_id is not a integer but a user instance!
+            landscape_likes.add(like.user_id)
+        #landscape_likes is a set of users
+        context_dict['landscape_likes'] = landscape_likes
+    except saved_landscapes.DoesNotExist:
         # do nothing
-        context_dict['profile_likes'] = None
+        context_dict['landscape_likes'] = None
     return render(request, 'profile/profile.html', context=context_dict)
 
-# try to filter reviews made by user
-@login_required()
-def show_profile_reviews(request):
+# try to filter users who reviewed this landscape
+def show_landscape_reviews(request):
     # Create a context dictionary which we can pass
     # to the template rendering engine.
     context_dict = {}
+    landscape_review = set()
     try:
-        profile_reviews = Review.objects.filter(
-            user_id=request.user.id).order_by('-visit_date')
-        for r in profile_reviews:
-            r.rating = roundRating(r.rating)
-        context_dict['profile_reviews'] = profile_reviews
-    except Review.DoesNotExist:
+        saved_landscape_reviews = Review.objects.filter(
+            landscpae_id=request.landscape.id)
+        for review in saved_landscape_reviews:
+            #like.user_id is not a integer but a user instance!
+            landscape_review.add(review.user_id)
+        #landscape_reviews is a set of users
+        context_dict['landscape_review'] = landscape_review
+    except saved_landscapes.DoesNotExist:
         # do nothing
-        context_dict['profile_reviews'] = None
+        context_dict['landscape_review'] = None
     return render(request, 'profile/profile.html', context=context_dict)
